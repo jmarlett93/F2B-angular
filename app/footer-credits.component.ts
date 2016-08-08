fb9042bf34aa3633265a07bf591b4afccba3d8c7
John Marlett <1fiyero@gmail.com>
2016-08-07 21:49:29 -0500

    adding in HTML template implementation

---
+++ app/footer-credits.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'Footer-Credits',
+    templateUrl: 'app/footer-credits.component.html',
+    styleUrls: ['app/footer-credits.component.css']
+
+})
+
+    export class FooterCredits { }
--- app/home-page.component.ts
+++ app/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { F2Navigator} from 'app/F2-navigator.component';
 import { F2Branding } from 'app/F2-branding.component';
 import { F2SiteInfo } from 'app/F2-siteinfo.component';
+import { FooterCredits } from 'app/footer-credits.component';
 
 @Component ({
     selector: 'home-page',
     templateUrl: 'app/home-page.component.html',
-    directives: [F2Navigator, F2Branding, F2SiteInfo]
+    directives: [F2Navigator, F2Branding, F2SiteInfo, FooterCredits ]
 })
 
 export class HomePage { }
