62c8743be4778a9670132ba44ab2fe69c43a6e51
John Marlett <1fiyero@gmail.com>
2016-08-26 18:32:20 -0500

    deleted duplicate main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,10 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { disableDeprecatedForms, provideForms} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { appRouterProviders } from './app.routes';
 
-bootstrap(AppComponent, [
-    disableDeprecatedForms(),
-    provideForms(),
-    appRouterProviders,
-]).catch(err => console.error(err)); 
-
-/*/ The browser platform with a compiler
+// The browser platform with a compiler
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 // The app module
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 // Compile and launch the module
 platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err));;
-*/
