479a3adadf0452579d608f03edb62da81421924f
John Marlett <1fiyero@gmail.com>
2016-08-29 16:59:05 -0500

    changed container trying to fix

--- app/F2-contactcontainer.component.ts
+++ app/F2-contactcontainer.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 import { SellerContactComponent } from './contact-form.component';
 
 @Component ({
--- app/contact-page.component.ts
+++ app/contact-page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { F2Navigator} from './F2-navigator.component';
 import { FooterCredits } from './footer-credits.component';
+import { SellerContactComponent } from './contact-form.component';
 import { F2ContactContainer} from './F2-contactcontainer.component';
+import { F2FormContainer } from './form-container.component';
 
 @Component({
     selector: 'contact-page',
     templateUrl: 'app/contact-page.component.html',
-    directives: [ F2Navigator, FooterCredits, F2ContactContainer ]
+    directives: [ F2Navigator, FooterCredits, F2ContactContainer, F2FormContainer, SellerContactComponent ]
 })
 
 export class ContactPage {
