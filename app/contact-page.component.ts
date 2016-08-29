c588c52cbbd2fb92454625c715b67cc0cafa8b6a
John Marlett <1fiyero@gmail.com>
2016-08-29 18:09:00 -0500

    edited contact-page

--- app/contact-page.component.ts
+++ app/contact-page.component.ts
@@ -2,13 +2,12 @@ import { Component } from '@angular/core';
 import { F2Navigator} from './F2-navigator.component';
 import { FooterCredits } from './footer-credits.component';
 import { SellerContactComponent } from './contact-form.component';
-import { F2ContactContainer} from './F2-contactcontainer.component';
-import { F2FormContainer } from './form-container.component';
 
 @Component({
     selector: 'contact-page',
     templateUrl: 'app/contact-page.component.html',
-    directives: [ F2Navigator, FooterCredits, F2ContactContainer, F2FormContainer, SellerContactComponent ]
+    styleUrls: ['app/contact-page.component.css'],
+    directives: [ F2Navigator, FooterCredits, SellerContactComponent ]
 })
 
 export class ContactPage {
